c127b5dbd0c1621f92d299708b9caeda20f7fcd8
dsw61130 <dsw61130@student.dsw.edu.pl>
2026-03-18 10:29:00 +0100

    movment logic improovment

--- Assets/PlayerMotor.cs
+++ Assets/PlayerMotor.cs
@@ -5,19 +5,54 @@ using UnityEngine.InputSystem;
 public class PlayerMotor : MonoBehaviour
 {
     Vector2 direction;
+    Rigidbody2D rigidbody2D;
     public float speed = 10;
+    public float jumpForce = 10;
+    private bool canJump = true;
+    private float maxSpeed = 5;
+    public float stoppingForce = 5;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.position += new Vector3(direction.x, direction.y, 0) * Time.deltaTime * speed;
+        if (direction.x != 0)
+        {
+            rigidbody2D.AddForce(new Vector2(direction.x * speed, 0));
+        }
+        else if (rigidbody2D.linearVelocityX != 0)
+        {
+            rigidbody2D.AddForce(new Vector2(-rigidbody2D.linearVelocityX * stoppingForce, 0));
+        }
+        if (rigidbody2D.linearVelocityX >= maxSpeed)
+        {
+            rigidbody2D.linearVelocityX = maxSpeed;
+        }
+        else if (rigidbody2D.linearVelocityX <= -maxSpeed)
+        {
+            rigidbody2D.linearVelocityX = -maxSpeed;
+        }
+        //transform.position += new Vector3(direction.x, direction.y, 0) * Time.deltaTime * speed;
     }
 
+    void OnJump()
+    {
+        if (canJump)
+        {
+            rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            canJump = false;
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        canJump = true;
+    }
     void OnMove(InputValue value)
     {
         //Debug.Log("move");
