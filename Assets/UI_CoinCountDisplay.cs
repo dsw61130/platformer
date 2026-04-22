f6239185d762cbf6b095b303b7e8007670823d42
dsw61130 <dsw61130@student.dsw.edu.pl>
2026-04-22 10:37:09 +0200

    yyyy

--- Assets/UI_CoinCountDisplay.cs
+++ Assets/UI_CoinCountDisplay.cs
@@ -7,7 +7,7 @@ public class UI_CoinCountDisplay : MonoBehaviour
     public coinComponent coinComponent;
     public TextMeshProUGUI textComponent;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
         coinComponent.OnCoinCountInitialised += OnCoinCountInitialised;
         coinComponent.OnCoinCount += OnCoinCount;
